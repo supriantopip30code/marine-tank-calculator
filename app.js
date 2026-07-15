5e28b41c8079b0cff67242e3cdc42dfabb324c37
supriantopip30code <suprianto.pip30@gmail.com>
2026-07-15 14:56:19 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// ====================================
+// Marine Tank Calculator
+// Version 0.1
+// Created by Chief Engineer Suprianto
+// ====================================
+
+console.log("Marine Tank Calculator Started");
+
+const app = {
+    version: "0.1",
+    vessel: "MV Gouta Maru",
+    author: "Chief Engineer Suprianto"
+};
+
+console.log(app);
