90cf61a9540de5b7b9412275ddb503ebeae18df0
supriantopip30code <suprianto.pip30@gmail.com>
2026-07-15 15:42:35 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "tank-calculator-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
